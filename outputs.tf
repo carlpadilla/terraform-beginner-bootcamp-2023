f251d2c601bbb90b0fb8bf7216101f4d20313f4a
Carl Padilla <36248459+carlpadilla@users.noreply.github.com>
2023-10-18 14:09:48 -0400

    #25 refactored s3 bucket into new terrahouse module (#26)

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "bucket_name" {
-  value = aws_s3_bucket.website_bucket.bucket
+  description = "Bucket name for static site"
+  value = module.terrahouse_aws.bucket_name
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,7 @@
 variable "user_uuid" {
-  description = "User UUID"
-  type        = string
-
-  validation {
-    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.user_uuid))
-    error_message = "Invalid user UUID format. It should be in the format of a UUID, e.g., '123e4567-e89b-12d3-a456-426655440000'."
-  }
+    type = string
 }
 
 variable "bucket_name" {
-  description = "Name of the AWS S3 bucket"
-  type        = string
-
-  validation {
-    condition     = can(regex("^[-a-z0-9]+$", var.bucket_name))
-    error_message = "Invalid bucket name. Bucket names must consist of only lowercase letters, numbers, and hyphens (-)."
-  }
+    type = string
 }
