aeb57a11040f47a2da276d7b1ccb91495b6def43
Carl Padilla <carlpadilla@protonmail.com>
2023-10-19 19:40:54 +0000

    #27 updated main.tf

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -34,13 +34,13 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
   
 }
 
-resource "aws_s3_bucket_object" "object" {
+resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
-  source = "/workspace/terraform-beginner-bootcamp-2023/modules/terrahouse_aws/main.tf"
+  source = "${path.root}/public/index.html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  #etag = filemd5("path/to/file")
+  etag = filemd5("${path.root}/public/index.html")
 }
