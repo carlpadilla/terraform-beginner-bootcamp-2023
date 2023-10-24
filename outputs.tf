05f9bf0be5ef645cb0c7e6bf0f471934749c0b32
Carl Padilla <carlpadilla@protonmail.com>
2023-10-24 16:31:20 +0000

    #27 saving updates, added resource to upload index.html, need to update variables next.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "Bucket name for static site"
   value = module.terrahouse_aws.bucket_name
 }
+
+output "s3_website_endpoint" {
+  description = "Bucket name for our static site"
+  value = module.terrahouse_aws.website_endpoint
+}
